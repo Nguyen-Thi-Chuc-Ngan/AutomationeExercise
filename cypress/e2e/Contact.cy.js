86987f2a6e8d3860b99953dcb731a9a8e048048f
Chuc-ngan <ngannyn16122003@gmail.com>
2025-04-27 15:15:33 +0700

    Add feature contact us

--- cypress/e2e/Contact.cy.js
+++ cypress/e2e/Contact.cy.js
@@ -1,7 +1,38 @@
+import 'cypress-file-upload';
+
 describe('', ()=>{
+
+    const email = 'testuser1745465282598@example.com';
+    const password = '1235678';
+    const username = "TestUser";
+    const filePath = 'text.txt';
+
     it('Contact Us Form', ()=>{
-        
-        
+        cy.visit('https://www.automationexercise.com/')
+        cy.url().should('include', 'automationexercise.com')
+        cy.get('body').should('be.visible')
+        cy.contains('Contact us').click()
+        cy.contains('Get In Touch').should('be.visible')
+
+        cy.get('input[data-qa="name"]').type(username)
+        cy.get('input[data-qa="name"]').should('have.value', username)
+
+        cy.get('input[data-qa="email"]').type(email)
+        cy.get('input[data-qa="email"]').should('have.value', email)
+
+        cy.get('input[data-qa="subject"]').type('Test subject')
+        cy.get('input[data-qa="subject"]').should('have.value', 'Test subject')
+
+        cy.get('textarea[data-qa="message"]').type('Test message')
+        cy.get('textarea[data-qa="message"]').should('have.value', 'Test message')
+
+        cy.get('input[type="file"]').attachFile(filePath);
+        cy.get('input[type="file"]').should('have.value', `C:\\fakepath\\${filePath}`);
+
+        cy.get('input[data-qa="submit-button"]').click()
+        cy.contains('Success! Your details have been submitted successfully.').should('be.visible')
+        cy.get('.btn.btn-success').click();
+        cy.url().should('include', 'automationexercise.com')
     })
 
 })
